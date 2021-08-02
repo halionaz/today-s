5aa7df6be837e18e5c8a3fb46585fd7d67e190f8
haSaJa <samhsj@naver.com>
2021-08-02 20:32:11 +0900

    CORS 정책에 막혀버린 미세먼지 농도 구현

--- js/todayDust.js
+++ js/todayDust.js
@@ -1,6 +1,17 @@
 
 // 구현 쉽지 않네
 
-function getDust(coords){
+const serviceKey = `3UGtVGK5YBQl7uvBFfwhqhCj0SixVvmtZCoSH3Prb19cBGcfGqC3RsyxfzzGnE%2FR3aw6GF%2Bjo560qqYnhkqwTA%3D%3D`;
 
+//시발 개같은 CORS 정책 위반 ㅅㅂㅅㅂㅅㅂㅅㅂ
+
+function getDust(coords){
+    let whatFor = 'getMsrstnAcctoRltmMesureDnsty';
+    let location = '서초구'
+    const dustDataLink = `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/${whatFor}?stationName=${location}&dataTerm=daily&pageNo=1&numOfRows=100&returnType=json&serviceKey=${serviceKey}`;
+    // $.getJSON(dustDataLink,data => {
+    //     console.log(data);
+    // });
 }
+
+getDust();
